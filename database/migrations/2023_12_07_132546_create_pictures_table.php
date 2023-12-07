09a502380ea934bfbfed2823af9fba35f8e99de9
vsantidev <vsantidev@gmail.com>
2023-12-07 17:15:33 +0100

    modification table pictures

--- database/migrations/2023_12_07_132546_create_pictures_table.php
+++ database/migrations/2023_12_07_132546_create_pictures_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('pictures', function (Blueprint $table) {
             $table->id();
             $table->string('name_picture');
+            $table->timestamps();
         });
     }
 
