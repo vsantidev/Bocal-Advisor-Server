470e77bf6afbf5a53dfbefd11f9435b41e60f30c
Zaïnab <zeinab0695@gmail.com>
2023-12-11 15:05:10 +0100

    routes

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 // Places
-Route::get('/post', [PlaceController::class, 'index']);
+// Route::get('/post', [PlaceController::class, 'index']);
 // Route::post('/post', [PlaceController::class, 'create']);
 // Route::post('/post/{id}', [PlaceController::class, 'show']);
 // Route::post('/post', [PlaceController::class, 'store']);
@@ -22,3 +22,4 @@ Route::get('/post', [PlaceController::class, 'index']);
 // Route::delete('/post', [PlaceController::class, 'destroy']);
 
 Route::post('/post', [PlaceController::class, 'place']);
+Route::get('/post', [PlaceController::class, 'renderPlace']);
