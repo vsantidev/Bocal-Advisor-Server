c6708d5f293973ffc55eb07f4c32e4edb0278320
vsantidev <vsantidev@gmail.com>
2023-12-11 15:10:10 +0100

    controller review

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -4,62 +4,32 @@ namespace App\Http\Controllers;
 
 use App\Models\Review;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class ReviewController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function review(Request $request)
     {
-        //
+        $request -> validate([
+            "comment" => "required|string",
+            "rate" => "required|integer"
+        ]);
+        
+        // CRÉATION DU COMMENTAIRE DANS LA BD ET RENVOIE JSON POUR FRONT
+        // $user_id = Auth::id();
+        $newReview = [
+            'comment' => $request->comment,
+            'rate' => $request->rate,
+            // 'user_id' => $user_id,
+            // 'place_id' => $request->place_id
+        ];
+       
+        Review::create($newReview);
+
+        return response()->json([
+            $newReview
+        ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Review $review)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Review $review)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Review $review)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Review $review)
-    {
-        //
-    }
 }
