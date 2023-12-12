2f27f7372c1817b83d8ae1f2f4d8648023e21a4c
Zaïnab <zeinab0695@gmail.com>
2023-12-12 19:59:43 +0100

    debugging

--- app/Http/Controllers/PlaceController.php
+++ app/Http/Controllers/PlaceController.php
@@ -15,11 +15,11 @@ class PlaceController extends Controller
 
     public function renderPlace()
     {
-        $tests = Place::all();
-        foreach ($tests as $test) {
-            $category = $test->categories->name_category;
-            dd($test);
-        }
+        // $tests = Place::all();
+        // foreach ($tests as $test) {
+        //     $category = $test->categories->name_category;
+        //     dd($test);
+        // }
         // Récupère tous les endroits enregistrés dans la bdd et les renvoie en format json
         $place = Place::all()->sortBy('name');
         return response()->json([
