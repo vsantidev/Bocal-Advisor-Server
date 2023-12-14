9d8b240ca8477052cafc941ae75983a1a06d2bc6
vsantidev <vsantidev@gmail.com>
2023-12-14 10:23:01 +0100

    modification table pictures 2

--- database/migrations/2023_12_07_132615_create_pictures_table.php
+++ database/migrations/2023_12_07_132615_create_pictures_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Place;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,8 +14,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('pictures', function (Blueprint $table) {
-            $table->id();
-            $table->string('name_picture');
+            $table->increments('id');
+            $table->string('name');
+            $table->foreignIdFor(User::class)->constrained();
+            $table->foreignIdFor(Place::class)->constrained();
             $table->timestamps();
         });
     }
