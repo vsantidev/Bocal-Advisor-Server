572ea7f0ade51fbf52ae8959180e716789d6b508
Dennys5 <dennys.rocca07@gmail.com>
2023-12-14 09:53:26 +0100

    dashboard

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,9 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::get('dashboard', [AuthController::class, 'dashboard'])
-    ->middleware('auth:sanctum');
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('dashboard', [AuthController::class, 'dashboard']);
+});
 
 // Places
 // Route::get('/post', [PlaceController::class, 'index']);
