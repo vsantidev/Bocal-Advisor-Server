537846f29c1a05a0aeb4a0f252386b4771e697c5
vsantidev <vsantidev@gmail.com>
2023-12-14 10:12:51 +0100

    relation model

--- app/Models/Picture.php
+++ app/Models/Picture.php
@@ -4,10 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Picture extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name_picture'];
+    protected $fillable = ['name'];
+
+
+    
+    // RELATION AVEC LA TABLE REVIEW
+    public function reviews() : HasOne
+    {
+        return $this->hasOne(Review::class);
+    }
+
+    
+    // RELATION AVEC LA TABLE REVIEW
+    public function places() : HasOne
+    {
+        return $this->hasOne(Place::class);
+    }
 }
